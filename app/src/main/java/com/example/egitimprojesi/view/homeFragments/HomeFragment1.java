faff516d0fa0f8d4d4b379efbd9ed157c3ca9cc2
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2021-01-05 18:48:54 +0300

    onClick event added to home main fragment buttons

--- app/src/main/java/com/example/egitimprojesi/view/homeFragments/HomeFragment1.java
+++ app/src/main/java/com/example/egitimprojesi/view/homeFragments/HomeFragment1.java
@@ -7,16 +7,76 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.example.egitimprojesi.R;
+import com.example.egitimprojesi.view.homeFragments.homeButtonsFragments.BilgiKartlari;
+import com.example.egitimprojesi.view.homeFragments.homeButtonsFragments.DenemeTakibi;
+import com.example.egitimprojesi.view.homeFragments.homeButtonsFragments.KonuTakibi;
+import com.example.egitimprojesi.view.homeFragments.homeButtonsFragments.Odaklanma;
+import com.example.egitimprojesi.view.homeFragments.homeButtonsFragments.SoruTakibi;
+import com.example.egitimprojesi.view.homeFragments.homeButtonsFragments.YildizliSorular;
+import com.example.egitimprojesi.view.mainFragments.HomeMain;
+import com.google.android.material.card.MaterialCardView;
 
 public class HomeFragment1 extends Fragment  {
 
+    CardView btn1,btn2,btn3,btn4,btn5,btn6;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.home_page_1,container,false);
+
+        btn1=rootView.findViewById(R.id.homeBtn1);
+        btn2=rootView.findViewById(R.id.homeBtn2);
+        btn3=rootView.findViewById(R.id.homeBtn3);
+        btn4=rootView.findViewById(R.id.homeBtn4);
+        btn5=rootView.findViewById(R.id.homeBtn5);
+        btn6=rootView.findViewById(R.id.homeBtn6);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new KonuTakibi()).commit();
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new SoruTakibi()).commit();
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new DenemeTakibi()).commit();
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new BilgiKartlari()).commit();
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new YildizliSorular()).commit();
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new Odaklanma()).commit();
+            }
+        });
+
         return rootView;
     }
 }
