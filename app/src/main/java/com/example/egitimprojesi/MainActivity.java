bdf6d3b313db31a8c54e63ae29ff6fd6a544a57a
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2021-01-05 18:09:17 +0300

    initial commit

---
+++ app/src/main/java/com/example/egitimprojesi/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.egitimprojesi;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.egitimprojesi.adapter.MainTabsPagerAdapter;
+import com.example.egitimprojesi.view.mainFragments.ForumMain;
+import com.example.egitimprojesi.view.mainFragments.HomeMain;
+import com.example.egitimprojesi.view.mainFragments.ProgramMain;
+import com.example.egitimprojesi.view.mainFragments.SettingsMain;
+import com.example.egitimprojesi.view.mainFragments.ToDoMain;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new HomeMain()).commit();
+        bottomNavigationView = findViewById(R.id.bottomNavigationBar);
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment seleceted = null;
+                switch (item.getItemId()) {
+                    case R.id.item1:
+                        seleceted = new HomeMain();
+                        break;
+                    case R.id.item2:
+                        seleceted = new ToDoMain();
+                        break;
+                    case R.id.item3:
+                        seleceted = new ProgramMain();
+                        break;
+                    case R.id.item4:
+                        seleceted = new ForumMain();
+                        break;
+                    case R.id.item5:
+                        seleceted = new SettingsMain();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, seleceted).commit();
+                return true;
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/egitimprojesi/view/mainFragments/HomeMain.java
@@ -0,0 +1,46 @@
+package com.example.egitimprojesi.view.mainFragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.egitimprojesi.R;
+import com.example.egitimprojesi.adapter.HomePagerAdapter;
+import com.example.egitimprojesi.view.homeFragments.HomeFragment1;
+import com.example.egitimprojesi.view.homeFragments.HomeFragment2;
+import com.example.egitimprojesi.view.homeFragments.HomeFragment3;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeMain extends Fragment {
+
+    private ViewPager pager;
+    private PagerAdapter pagerAdapter;
+    private TabLayout tabLayout;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.home,container,false);
+        List<Fragment> fragmentList = new ArrayList<>();
+        fragmentList.add(new HomeFragment1());
+        fragmentList.add(new HomeFragment2());
+        fragmentList.add(new HomeFragment3());
+
+        pager=rootView.findViewById(R.id.pager);
+        pagerAdapter = new HomePagerAdapter(getFragmentManager(),fragmentList);
+        pager.setAdapter(pagerAdapter);
+
+        tabLayout=rootView.findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(pager);
+        return rootView;
+    }
+}
